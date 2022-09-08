937c189a427405d4bb46db46a94da1ca1a87b2b0
Riya Basak <riyabasak26@gmail.com>
2022-09-08 11:34:11 +0530

    PR comment resolve

--- app.js
+++ app.js
@@ -2,17 +2,17 @@ let myTodo = [];
 let taskTitle = "";
 let isInEditMode = false;
 /*----------DOM element reference----------*/
-let todoInputBox = document.getElementById("todo-input");
-let addButton = document.getElementById("add-todo");
-let activeTodoList = document.getElementById("todo-item");
-let completedTodoList = document.getElementById("CompletedTodo");
-let addEditedTodoButton = document.getElementById("add-edited-todo");
+const todoInputBox = document.getElementById("todo-input");
+const addButton = document.getElementById("add-todo");
+const activeTodoList = document.getElementById("todo-item");
+const completedTodoList = document.getElementById("CompletedTodo");
+const addEditedTodoButton = document.getElementById("add-edited-todo");
 /*----------DOM element reference----------*/
 let selectedIndex = -1;
 
 const onChangeHandler = (event) => {
   if (event.key === "Enter") {
-    if (selectedIndex == -1) {
+    if (selectedIndex === -1) {
       createToDo();
     } else {
       replaceTodo();
@@ -22,8 +22,8 @@ const onChangeHandler = (event) => {
     taskTitle = title;
   }
 };
-/*-------it help to push the todo into array---------*/
-const createToDo=()=> {
+/*-------it help to push the todos into array---------*/
+const createToDo = () => {
   if (taskTitle.length) {
     myTodo.push({
       title: taskTitle,
@@ -34,9 +34,9 @@ const createToDo=()=> {
   } else {
     alert("You must write something!");
   }
-}
-/*-------it help to show the todo in list format--------*/
-const showTodo=()=> {
+};
+/*-------it help to show the todos in list format--------*/
+const showTodo = () => {
   let data = "";
   const totalNumberOfTODOs = myTodo.length - 1;
   const todoToBeAdded = myTodo[totalNumberOfTODOs];
@@ -54,61 +54,61 @@ const showTodo=()=> {
   li.id = totalNumberOfTODOs;
   li.innerHTML = data;
   document.getElementById("todo-item").appendChild(li);
-}
-/*-----------it help to checked the todo and remove it from the list------------*/
-const checkedTodo=(index)=> {
-  let Todospan = document.querySelector(`.Todo-${index}`);
-  Todospan.classList.add("strike");
+};
+/*-----------it help to checked the todos and remove it from the list------------*/
+const checkedTodo = (index) => {
+  const todoSpan = document.querySelector(`.Todo-${index}`);
+  todoSpan.classList.add("strike");
   setTimeout(function () {
-  let checkedTodoValue = myTodo[index].title;
-  let checkedTodoList = document.createElement("li");
-  checkedTodoList.id = "completed-Todo-list";
-  checkedTodoList.appendChild(document.createTextNode(checkedTodoValue));
-  completedTodoList.appendChild(checkedTodoList);
-  let deleteButton = document.createElement("button");
-  deleteButton.innerHTML = "delete";
-  deleteButton.className = "material-symbols-outlined icon";
-  deleteButton.addEventListener("click", deleteTheCheckedTodo);
-  checkedTodoList.appendChild(deleteButton);
-  let list = document.getElementById(index);
-  list.remove();
+    const checkedTodoValue = myTodo[index].title;
+    const checkedTodoList = document.createElement("li");
+    checkedTodoList.id = "completed-Todo-list";
+    checkedTodoList.appendChild(document.createTextNode(checkedTodoValue));
+    completedTodoList.appendChild(checkedTodoList);
+    const deleteButton = document.createElement("button");
+    deleteButton.innerHTML = "delete";
+    deleteButton.className = "material-symbols-outlined icon";
+    deleteButton.addEventListener("click", deleteTheCheckedTodo);
+    checkedTodoList.appendChild(deleteButton);
+    const list = document.getElementById(index);
+    list.remove();
   }, 500);
-}
-/*-----it help to delete the todo from list------ */
-const deleteTodo=(index) =>{
-  if (confirm("Do you want to delete this item.") == true) {
+};
+/*-----it help to delete the todos from list------ */
+const deleteTodo = (index) => {
+  if (confirm("Do you want to delete this item?") === true) {
     let todo = document.getElementById(index);
     todo.remove();
   } else {
-    return
+    return;
   }
-}
+};
 /*-----it help to delete the todo from checked todos----  */
-const deleteTheCheckedTodo=()=> {
-  if (confirm("Do you want to delete this item.") == true) {
+const deleteTheCheckedTodo = () => {
+  if (confirm("Do you want to delete this item?") === true) {
     document.getElementById("completed-Todo-list").remove();
   } else {
-    return 
+    return;
   }
-}
+};
 /*------on click on edit button it will go to edit mode-----*/
-const editTodo=(index)=> {
+const editTodo = (index) => {
   selectedIndex = index;
   document.getElementById("todo-input").value = myTodo[index].title;
   addEditedTodoButton.style.display = "block";
   addButton.style.display = "none";
-}
+};
 /*-------it will replace the existing todo with the edited one on click on add button-----*/
-const replaceTodo=()=> {
+const replaceTodo = () => {
   addEditedTodoButton.style.display = "none";
   addButton.style.display = "block";
-  let editedTodo = todoInputBox.value;
+  const editedTodo = todoInputBox.value;
   myTodo[selectedIndex].title = editedTodo;
-  let span = document.querySelector(`.Todo-${selectedIndex}`);
+  const span = document.querySelector(`.Todo-${selectedIndex}`);
   span.innerHTML = editedTodo;
   selectedIndex = -1;
   document.getElementById("todo-input").value = "";
-}
+};
 
 /*----------Event Listener Attachment----------*/
 todoInputBox.addEventListener("keyup", onChangeHandler);
